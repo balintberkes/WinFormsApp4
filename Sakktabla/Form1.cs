cfc3122f948d48143a69dc825bd197225ece3580
Bálint Berkes <balint.berkes@stud.uni-corvinus.hu>
2024-03-01 11:11:04 +0100

    blabla

---
+++ Sakktabla/Form1.cs
@@ -0,0 +1,38 @@
+namespace Sakktabla
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Button g = new Button();
+                    g.Left = i * 40;
+                    g.Top = j * 40;
+                    g.Width = 40;
+                    g.Height = 40;
+                    g.Text = (i * j).ToString();
+
+                    if (i % 2 == 0)
+                    {
+                        g.Visible = true;
+                    }
+
+                    else
+                    { 
+                        g.Visible=false;
+                    }
+
+                    Controls.Add(g);
+                }
+            }
+        }
+    }
+}
