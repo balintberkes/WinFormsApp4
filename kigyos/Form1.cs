937051cd7fb3f5e0c3beda3dee39dcdd200cfc2f
qttvy2 <qttvy2@SZA0030875>
2024-03-21 10:37:02 +0100

    bla

---
+++ kigyos/Form1.cs
@@ -0,0 +1,83 @@
+namespace kigyos
+{
+    public partial class Form1 : Form
+    {
+        int fej_x = 100;
+        int fej_y = 100;
+
+        int irány_x = 1;
+        int irány_y = 0;
+
+        int lépésszám;
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+
+        int hossz;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lépésszám++;
+
+            //Fejnövesztés
+            fej_x += irány_x * KígyóElem.Méret;
+            fej_y += irány_y * KígyóElem.Méret;
+
+            foreach (KígyóElem item in Controls)
+            {
+                //Ha vannak már valami ott, ahova az új fejet tenném, vége a játéknak
+                if (item.Top == fej_y && item.Left == fej_x)
+                {
+                    timer1.Enabled = false;
+                    return;
+                }
+            }
+
+            KígyóElem ke = new KígyóElem();
+            ke.Top = fej_y;
+            ke.Left = fej_x;
+            Controls.Add(ke);
+
+            
+
+            if (Controls.Count > hossz) Controls.RemoveAt(0);
+            if (lépésszám % 5 == 0) hossz++;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                irány_y = -1;
+                irány_x = 0;
+            }
+
+            if (e.KeyCode == Keys.Down)
+            {
+                irány_y = 1;
+                irány_x = 0;
+            }
+
+            if (e.KeyCode == Keys.Left)
+            {
+                irány_y = 0;
+                irány_x = -1;
+            }
+
+            if (e.KeyCode == Keys.Right)
+            {
+                irány_y = 0;
+                irány_x = 1;
+            }
+        }
+
+        
+
+
+
+
+    }
+}
